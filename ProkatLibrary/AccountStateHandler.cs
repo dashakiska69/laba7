08b3df06a85eee58ecefce333592df9a9613e399
dashakiska69 <dashakiska69@gmail.com>
2018-10-29 00:21:17 +0300

    Создали библеотеку классов, добавили делагат  AccountStateHandler.cs и вспомогательный класс AccountEventArgs и интерфейс  IAccount, так же добавили абстрктный класс Account, который реализует интерфейс IAccount и определяет ряд событий, которые вызываются при изменении состояния.

---
+++ ProkatLibrary/AccountStateHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProkatLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        //Сообщение
+        public string Message { get; private set; }
+        //Сумма проката
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Sum = _sum;
+        }
+    }
+}
---
+++ ProkatLibrary/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProkatLibrary
+{
+    interface IAccount
+    {
+        //Вернуть ченить
+        void Put(decimal sum);
+        //Заплатить за аренду
+        decimal Withdraw(decimal sum);
+
+    }
+}
